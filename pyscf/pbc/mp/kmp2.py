97681ae29725bfb653a77864cd91dc22808db23a
ethanvo <ethan.vo@hotmail.com>
2024-02-23 11:10:13 -0500

    t2 fix?

--- pyscf/pbc/mp/kmp2.py
+++ pyscf/pbc/mp/kmp2.py
@@ -681,12 +681,10 @@ def _gamma1_intermediates(mp, t2=None):
         for kj in range(nkpts):
             for ka in range(nkpts):
                 kb = mp.khelper.kconserv[ki, ka, kj]
-                ijab = np.asarray(t2[ki, kj, ka])
-                ijba = np.asarray(t2[ki, kj, kb])
-                dm1vir[kb] += einsum('ijax,ijay->yx', ijab.conj(), ijab) * 2 -\
-                              einsum('ijax,ijya->yx', ijab.conj(), ijba)
-                dm1occ[kj] += einsum('ixab,iyab->xy', ijab.conj(), ijab) * 2 -\
-                              einsum('ixab,iyba->xy', ijab.conj(), ijba)
+                dm1vir[kb] += einsum('ijax,ijay->yx', t2[ki, kj, ka].conj(), t2[ki, kj, ka]) * 2 -\
+                              einsum('ijax,ijya->yx', t2[ki, kj, ka].conj(), t2[ki, kj, kb])
+                dm1occ[kj] += einsum('ixab,iyab->xy', t2[ki, kj, ka].conj(), t2[ki, kj, ka]) * 2 -\
+                              einsum('ixab,iyba->xy', t2[ki, kj, ka].conj(), t2[ki, kj, kb])
     return -dm1occ, dm1vir
 
 
